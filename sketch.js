bb2b4afe0137f9f887ff0b8ffe83cf8542cfd73b
Gabriel Guedes <ingabrielguedes@gmail.com>
2024-06-15 14:45:51 -0300

    adicionando os codigos do meu primeiro projeto

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(400, 400);
+    background("blue");
+  }
+  
+  function draw() {
+    
+    stroke("green");
+    fill("orange");
+    
+    // console.log(mouseIsPressed);
+    
+    if (mouseIsPressed){
+          rect(mouseX,mouseY,50,50);
+    }
+    
+  }
+  
+  
+  
